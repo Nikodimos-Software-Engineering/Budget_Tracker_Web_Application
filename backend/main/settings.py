02cc4350d7773b5d042b3184345a99d7eb94e853
Nikodimos-Hunegn <nikodimoshunegn17@gmail.com>
2025-12-13 10:01:12 -0800

    Use environment variables for SECRET_KEY and DEBUG
    Updated settings to use environment variables for SECRET_KEY and DEBUG.

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2i!&_8u)%u9(b!*yr!s6(&r^)md372q@plxv&9d^j*h^8=gx%_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = [
     "localhost",
